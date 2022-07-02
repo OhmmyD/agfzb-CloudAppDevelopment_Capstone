b45c3d715ab6ff5e37461f8015019d118209b75f
OhmmyD <ohm.doungkhae@gmail.com>
2022-07-02 00:32:45 +0000

    Week 3 changes part 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from .import views
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -27,6 +27,7 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealerId=<int:dealerId>', views.get_dealer_details, name='logout'),
 
     # path for add a review view
 
